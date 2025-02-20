9ea218e8fe22250bb9a693636699fd49b95b3cd6
asdfish <lgray3420@gmail.com>
2025-02-20 10:17:18 -0500

    finished vector stdlib procedures

--- src/vectors.rs
+++ src/vectors.rs
@@ -2,16 +2,20 @@ use crate::{
     exception::Exception, gc::Gc, lists::slice_to_list, num::Number, registry::bridge, value::Value,
 };
 use rug::Integer;
-use std::{clone::Clone, cmp::Ord, error::Error as StdError, fmt::Display, ops::Range};
+use std::{clone::Clone, error::Error as StdError, fmt::Display, ops::Range};
 
-fn range(start: usize, end: usize) -> Result<Range<usize>, Exception> {
+fn try_make_range(start: usize, end: usize) -> Result<Range<usize>, Exception> {
     if end < start {
-        Err(Exception::error(format!(
-            "Invalid range: start {start} is lesser than end {end}"
-        )))
+        return Err(Exception::error(format!(
+            "Range end {} cannot be less than start {}",
+            end, start
+        )));
     } else {
         Ok(start..end)
     }
+}
+fn try_to_u64(n: &Gc<Value>) -> Result<u64, Exception> {
+    n.read().as_ref().try_into().map(|n: &Number| n.to_u64())
 }
 
 trait Indexer {
@@ -21,14 +25,14 @@ trait Indexer {
     fn get_range(&self, _: &Self::Collection, _: Range<usize>) -> Self::Collection;
     fn try_get<'a>(&self, _: &'a Value) -> Result<&'a Self::Collection, Exception>;
 
-    fn index(&self, args: &[Gc<Value>]) -> Result<Self::Collection, Exception> {
-        fn try_to_u64(n: &Gc<Value>) -> Result<u64, Exception> {
-            n.read().as_ref().try_into().map(|n: &Number| n.to_u64())
-        }
-
+    fn index(
+        &self,
+        args: &[Gc<Value>],
+        arg_range: Range<usize>,
+    ) -> Result<Self::Collection, Exception> {
         let arg = args
             .first()
-            .ok_or_else(|| Exception::wrong_num_of_variadic_args(1..3, 0))?;
+            .ok_or_else(|| Exception::wrong_num_of_variadic_args(arg_range, 0))?;
         let arg = arg.read();
         let collection = self.try_get(&arg)?;
         let len = self.get_len(collection);
@@ -47,20 +51,15 @@ trait Indexer {
             .transpose()?
             .unwrap_or(len.try_into()?);
 
-        if end < start {
-            return Err(Exception::error(format!(
-                "Range end {} cannot be less than start {}",
-                end, start
-            )));
-        }
-        if start >= len {
+        let range = try_make_range(start, end)?;
+        if range.end >= len {
             return Err(Exception::invalid_range(
-                start.try_into()?..end.try_into()?,
+                range,
                 len,
             ));
         }
 
-        Ok(self.get_range(collection, start..end))
+        Ok(self.get_range(collection, range))
     }
 }
 
@@ -72,7 +71,8 @@ impl Indexer for StringIndexer {
         string.chars().count()
     }
     fn get_range(&self, string: &String, range: Range<usize>) -> String {
-        string.chars()
+        string
+            .chars()
             .skip(range.start)
             .take(range.end - range.start)
             .collect()
@@ -120,7 +120,7 @@ pub async fn make_vector(args: &[Gc<Value>]) -> Result<Vec<Gc<Value>>, Exception
     let with = args.get(1);
 
     Ok((0..n)
-        .map(|_| with.cloned().unwrap_or(Gc::new(Value::Null)))
+        .map(|_| with.cloned().unwrap_or_else(|| Gc::new(Value::Null)))
         .collect())
 }
 
@@ -187,14 +187,19 @@ pub async fn vector_set(
 
 #[bridge(name = "vector->list", lib = "(base)")]
 pub async fn vector_to_list(args: &[Gc<Value>]) -> Result<Vec<Gc<Value>>, Exception> {
-    let vec: Vec<Gc<Value>> = VectorIndexer.index(args)?.into_iter().map(Gc::new).collect();
+    let vec: Vec<Gc<Value>> = VectorIndexer
+        .index(args, 1..3)?
+        .into_iter()
+        .map(Gc::new)
+        .collect();
     Ok(vec![Gc::new(slice_to_list(vec.as_slice()))])
 }
 
 #[bridge(name = "vector->string", lib = "(base)")]
 pub async fn vector_to_string(args: &[Gc<Value>]) -> Result<Vec<Gc<Value>>, Exception> {
     Ok(vec![Gc::new(Value::String(
-        VectorIndexer.index(args)?
+        VectorIndexer
+            .index(args, 1..3)?
             .into_iter()
             .map(|val| <Value as TryInto<char>>::try_into(val))
             .collect::<Result<String, _>>()?,
@@ -203,7 +208,101 @@ pub async fn vector_to_string(args: &[Gc<Value>]) -> Result<Vec<Gc<Value>>, Exce
 
 #[bridge(name = "string->vector", lib = "(base)")]
 pub async fn string_to_vector(args: &[Gc<Value>]) -> Result<Vec<Gc<Value>>, Exception> {
-    Ok(vec![Gc::new(
-        Value::Vector(StringIndexer.index(args)?.chars().map(|c| Value::Character(c)).collect())
-    )])
+    Ok(vec![Gc::new(Value::Vector(
+        StringIndexer
+            .index(args, 1..3)?
+            .chars()
+            .map(|c| Value::Character(c))
+            .collect(),
+    ))])
+}
+
+#[bridge(name = "vector-copy", lib = "(base)")]
+pub async fn vector_copy(args: &[Gc<Value>]) -> Result<Vec<Gc<Value>>, Exception> {
+    Ok(vec![Gc::new(Value::Vector(
+        VectorIndexer.index(args, 1..3)?,
+    ))])
+}
+
+#[bridge(name = "vector-copy!", lib = "(base)")]
+pub async fn vector_copy_to(args: &[Gc<Value>]) -> Result<Vec<Gc<Value>>, Exception> {
+    const ARGS: Range<usize> = 2..5;
+
+    let mut args = args.iter();
+    let mut i = 0;
+    let mut next_arg = || {
+        let arg = args
+            .next()
+            .ok_or_else(|| Exception::wrong_num_of_variadic_args(ARGS, i));
+        i += 1;
+
+        arg
+    };
+
+    let mut to = next_arg()?.write();
+    let to: &mut Vec<Value> = to.as_mut().try_into()?;
+
+    let at: usize = try_to_u64(next_arg()?)?.try_into()?;
+
+    if at >= to.len() {
+        return Err(Exception::invalid_index(at, to.len()));
+    }
+    let split = to.split_off(at);
+
+    to.extend(
+        VectorIndexer
+            .index(args.as_slice(), ARGS)?
+            .into_iter()
+            .chain(split.into_iter()),
+    );
+
+    Ok(vec![])
+}
+
+#[bridge(name = "vector-append", lib = "(base)")]
+pub async fn vector_append(args: &[Gc<Value>]) -> Result<Vec<Gc<Value>>, Exception> {
+    if args.is_empty() {
+        return Err(Exception::wrong_num_of_variadic_args(1..usize::MAX, 0));
+    }
+
+    Ok(vec![Gc::new(Value::Vector(
+        args.iter()
+            .flat_map(|arg| {
+                <&Value as TryInto<&Vec<Value>>>::try_into(arg.read().as_ref())
+                    .into_iter()
+                    .cloned()
+                    .collect::<Vec<_>>()
+            })
+            .flatten()
+            .collect::<Vec<_>>(),
+    ))])
+}
+
+#[bridge(name = "vector-fill", lib = "(base)")]
+pub async fn vector_fill(
+    vector: &Gc<Value>,
+    with: &Gc<Value>,
+    start: &Gc<Value>,
+    end: &[Gc<Value>],
+) -> Result<Vec<Gc<Value>>, Exception> {
+    let mut vector = vector.write();
+    let vector: &mut Vec<Value> = vector.as_mut().try_into()?;
+
+    let start: usize = try_to_u64(start)?.try_into()?;
+    let end: usize = end.first().map(try_to_u64).transpose()?
+        .map(|n| n.try_into()).transpose()?
+        .unwrap_or(vector.len());
+    
+    let range = try_make_range(start, end)?;
+    if range.end > vector.len() {
+        return Err(Exception::invalid_range(range, vector.len()));
+    }
+
+    for i in range {
+        if let Some(i) = vector.get_mut(i) {
+            *i = with.read().as_ref().clone();
+        }
+    }
+
+    Ok(vec![])
 }
