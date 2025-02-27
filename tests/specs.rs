c88b4d3d4918c5fdc540c7b24f5a2ce6c9d22d7f
asdfish <lgray3420@gmail.com>
2025-02-26 19:16:36 -0500

    added failure testing

--- tests/specs.rs
+++ tests/specs.rs
@@ -11,6 +11,44 @@ use scheme_rs::{
     syntax::{Span, Syntax},
     value::{eqv, Value},
 };
+use std::error::Error as StdError;
+
+struct TestRuntime {
+    runtime: Gc<Runtime>,
+    test_top: Environment,
+}
+impl TestRuntime {
+    pub async fn new() -> Self {
+        let runtime = Gc::new(Runtime::new());
+        let registry = Registry::new(&runtime).await;
+        let base = registry.import("(base)").unwrap();
+        let mut test_top = Top::program();
+        {
+            let base = base.read();
+            test_top.import(&base);
+        }
+        let test_top = Environment::from(Gc::new(test_top));
+
+        Self {
+            runtime,
+            test_top,
+        }
+    }
+
+    pub async fn exec_syn(&self, sexprs: &[Syntax]) -> Result<(), Box<dyn StdError>> {
+        let base = DefinitionBody::parse_program_body(&self.runtime, &sexprs, &self.test_top, &Span::default())
+            .await
+            .unwrap();
+        let compiled = base.compile_top_level();
+        let closure = self.runtime.compile_expr(compiled).await.map_err(Box::new)?;
+        Ok(closure.call(&[]).await.map(drop).map_err(Box::new)?)
+    }
+
+    pub async fn exec_str<'a>(&self, src: &'a str) -> Result<(), Box<dyn StdError + 'a>> {
+        let sexprs = Syntax::from_str(src, None).map_err(Box::new)?;
+        self.exec_syn(&sexprs).await
+    }
+}
 
 #[bridge(name = "assert-eq", lib = "(base)")]
 pub async fn test_assert(arg1: &Gc<Value>, arg2: &Gc<Value>) -> Result<Vec<Gc<Value>>, Exception> {
@@ -23,34 +61,34 @@ pub async fn test_assert(arg1: &Gc<Value>, arg2: &Gc<Value>) -> Result<Vec<Gc<Va
     }
 }
 
-macro_rules! test_specification {
+#[macro_export]
+macro_rules! assert_file {
     ($name:ident) => {
-        #[tokio::test]
+        #[::tokio::test]
         async fn $name() {
-            let runtime = Gc::new(Runtime::new());
-            let registry = Registry::new(&runtime).await;
-            let base = registry.import("(base)").unwrap();
-            let mut test_top = Top::program();
-            {
-                let base = base.read();
-                test_top.import(&base);
-            }
-            let test_top = Environment::from(Gc::new(test_top));
-
-            let sexprs = Syntax::from_str(
+            let rt = $crate::TestRuntime::new().await;
+            let sexprs = $crate::Syntax::from_str(
                 include_str!(concat!(stringify!($name), ".scm")),
                 Some(concat!(stringify!($name), ".scm")),
-            )
-            .unwrap();
-            let base =
-                DefinitionBody::parse_program_body(&runtime, &sexprs, &test_top, &Span::default())
-                    .await
-                    .unwrap();
-            let compiled = base.compile_top_level();
-            let closure = runtime.compile_expr(compiled).await.unwrap();
-            closure.call(&[]).await.unwrap();
+            ).unwrap();
+            assert!(rt.exec_syn(&sexprs).await.inspect_err(|e| eprintln!("{}", e)).is_ok());
         }
     };
 }
-test_specification!(r6rs);
-test_specification!(r7rs);
+assert_file!(r6rs);
+assert_file!(r7rs);
+
+#[macro_export]
+macro_rules! assert_failure {
+    ($name:ident, $expr:literal) => {
+        #[::tokio::test]
+        async fn $name() {
+            let rt = $crate::TestRuntime::new().await;
+            assert!(rt.exec_str($expr).await
+            .inspect(|i| println!("{i:?}"))
+            .inspect_err(|i| println!("{i}"))
+                .is_err())
+        }
+    }
+}
+assert_failure!(byte_overflow, "#u8(9001)");
