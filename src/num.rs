9f3c2206b97f5f9868cacffe8758e39e001b80d7
asdfish <lgray3420@gmail.com>
2025-03-25 20:20:50 -0400

    fmt

--- src/num.rs
+++ src/num.rs
@@ -319,17 +319,23 @@ impl Number {
         Ok(match (self, rhs) {
             (l, r) if l.is_zero() || r.is_zero() => return Err(ArithmeticError::DivisionByZero),
 
-            (Self::FixedInteger(l), Self::FixedInteger(r)) => Self::Rational(Rational::from(*l) / Rational::from(*r)),
-            (Self::BigInteger(l), Self::BigInteger(r)) => Self::Rational(
-                Rational::from(l) / Rational::from(r)
-            ),
+            (Self::FixedInteger(l), Self::FixedInteger(r)) => {
+                Self::Rational(Rational::from(*l) / Rational::from(*r))
+            }
+            (Self::BigInteger(l), Self::BigInteger(r)) => {
+                Self::Rational(Rational::from(l) / Rational::from(r))
+            }
             (Self::Rational(l), Self::Rational(r)) => Self::Rational(l / r),
             (Self::Complex(l), Self::Complex(r)) => Self::Complex(l / r),
             (Self::Real(l), Self::Real(r)) => Self::Real(l / r),
 
-            (Self::BigInteger(l), Self::FixedInteger(r)) => Self::Rational(Rational::from(l) / Rational::from(*r)),
-            (Self::FixedInteger(l), Self::BigInteger(r)) => Self::Rational(Rational::from(*l) / Rational::from(r)),
-            
+            (Self::BigInteger(l), Self::FixedInteger(r)) => {
+                Self::Rational(Rational::from(l) / Rational::from(*r))
+            }
+            (Self::FixedInteger(l), Self::BigInteger(r)) => {
+                Self::Rational(Rational::from(*l) / Rational::from(r))
+            }
+
             (Self::Rational(l), Self::FixedInteger(r)) => Self::Rational(l / Rational::from(*r)),
             (Self::FixedInteger(l), Self::Rational(r)) => Self::Rational(Rational::from(*l) / r),
 
@@ -350,8 +356,12 @@ impl Number {
                 Self::Rational(Rational::try_from_float_simplest(*l)? / r)
             }
 
-            (Self::FixedInteger(l), Self::Real(r)) => Self::Rational(Rational::from(*l) / Rational::try_from_float_simplest(*r)?),
-            (Self::Real(l), Self::FixedInteger(r)) => Self::Rational(Rational::try_from_float_simplest(*l)? / Rational::from(*r)),
+            (Self::FixedInteger(l), Self::Real(r)) => {
+                Self::Rational(Rational::from(*l) / Rational::try_from_float_simplest(*r)?)
+            }
+            (Self::Real(l), Self::FixedInteger(r)) => {
+                Self::Rational(Rational::try_from_float_simplest(*l)? / Rational::from(*r))
+            }
 
             (Self::FixedInteger(l), Self::Complex(r)) => {
                 Self::Complex(Complex64::from_i64(*l).ok_or_else(overflow)? / r)
