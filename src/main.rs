b3fc310a52c8cb111bedd7aea89e56679bc467fd
asdfish <105313579+asdfish@users.noreply.github.com>
2025-03-31 00:53:22 +0000

    Replace `reedline` with `rustyline` (#102)
    * replaced `reedline` with `rustyline`
    
    * Bring back reedline features to rustyline
    
    ---------
    
    Co-authored-by: Matthew Plant <maplant@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use reedline::{Reedline, Signal, ValidationResult, Validator};
+use rustyline::{
+    error::ReadlineError,
+    highlight::MatchingBracketHighlighter,
+    history::DefaultHistory,
+    validate::{ValidationContext, ValidationResult, Validator},
+    Completer, Config, Editor, Helper, Highlighter, Hinter, Validator,
+};
 use scheme_rs::{
     ast::{DefinitionBody, ParseAstError},
     cps::Compile,
@@ -12,53 +18,50 @@ use scheme_rs::{
     syntax::Syntax,
     value::Value,
 };
-use std::borrow::Cow;
+use std::process::ExitCode;
 
-struct InputParser;
+#[derive(Default)]
+struct InputValidator;
 
-impl Validator for InputParser {
-    fn validate(&self, line: &str) -> ValidationResult {
-        let syntax = Syntax::from_str(line, None);
-        match syntax {
-            Err(ParseSyntaxError::UnclosedParen { .. }) => ValidationResult::Incomplete,
-            _ => ValidationResult::Complete,
+impl Validator for InputValidator {
+    fn validate(&self, ctx: &mut ValidationContext<'_>) -> rustyline::Result<ValidationResult> {
+        match Syntax::from_str(ctx.input(), None) {
+            Err(ParseSyntaxError::UnclosedParen { .. }) => Ok(ValidationResult::Incomplete),
+            _ => Ok(ValidationResult::Valid(None)),
         }
     }
 }
 
-struct Prompt;
-
-impl reedline::Prompt for Prompt {
-    fn render_prompt_left(&self) -> Cow<str> {
-        Cow::Borrowed("")
-    }
-
-    fn render_prompt_right(&self) -> Cow<str> {
-        Cow::Borrowed("")
-    }
-
-    fn render_prompt_indicator(&self, _prompt_mode: reedline::PromptEditMode) -> Cow<str> {
-        Cow::Borrowed("> ")
-    }
-
-    fn render_prompt_multiline_indicator(&self) -> Cow<str> {
-        Cow::Borrowed("  ")
-    }
-
-    fn render_prompt_history_search_indicator(
-        &self,
-        _history_search: reedline::PromptHistorySearch,
-    ) -> Cow<str> {
-        Cow::Borrowed("? ")
-    }
+#[derive(Completer, Helper, Highlighter, Hinter, Validator)]
+struct InputHelper {
+    #[rustyline(Validator)]
+    validator: InputValidator,
+    #[rustyline(Highlighter)]
+    highlighter: MatchingBracketHighlighter,
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> ExitCode {
     let runtime = Gc::new(Runtime::new());
     let registry = Registry::new(&runtime).await;
     let base = registry.import("(base)").unwrap();
-    let mut rl = Reedline::create().with_validator(Box::new(InputParser));
+
+    let config = Config::builder().auto_add_history(true).build();
+    let mut editor = match Editor::with_history(config, DefaultHistory::new()) {
+        Ok(e) => e,
+        Err(err) => {
+            eprintln!("Error creating line editor: {}", err);
+            return ExitCode::FAILURE;
+        }
+    };
+
+    let helper = InputHelper {
+        validator: InputValidator,
+        highlighter: MatchingBracketHighlighter::new(),
+    };
+
+    editor.set_helper(Some(helper));
+
     let mut n_results = 1;
     let mut repl = Top::repl();
     {
@@ -68,11 +71,16 @@ async fn main() {
     let top = Environment::from(Gc::new(repl));
 
     loop {
-        let Ok(Signal::Success(mut input)) = rl.read_line(&Prompt) else {
-            println!("exiting...");
-            return;
+        let input = match editor.readline("> ") {
+            Ok(line) => line,
+            Err(ReadlineError::Eof) => break,
+            Err(err) => {
+                eprintln!("Error while reading input: {}", err);
+                return ExitCode::FAILURE;
+            }
         };
-        input.push('\n');
+
+        //input.push('\n');
         match compile_and_run_str(&runtime, &top, &input).await {
             Ok(results) => {
                 for result in results.into_iter() {
@@ -85,6 +93,8 @@ async fn main() {
             }
         }
     }
+
+    ExitCode::SUCCESS
 }
 
 #[derive(derive_more::From, Debug)]
