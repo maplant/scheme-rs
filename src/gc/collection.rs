80d23a07314e1c6f3d382e1398bd97e9db4215d8
Matthew Plant <maplant@protonmail.com>
2025-11-19 21:57:43 -0500

    First fixes for MIRI

--- src/gc/collection.rs
+++ src/gc/collection.rs
@@ -59,7 +59,7 @@ impl GcHeader {
             color: Color::Black,
             buffered: true,
             visit_children: |this, visitor| unsafe {
-                let this = this as *const T;
+                let this = this as *const UnsafeCell<T> as *const T;
                 T::visit_or_recurse(this.as_ref().unwrap(), visitor);
             },
             finalize: |this| unsafe {
@@ -198,7 +198,7 @@ impl HeapObject<()> {
     }
 
     unsafe fn data(&self) -> *const () {
-        unsafe { self.data.as_ref().get() as *const () }
+        unsafe { self.data.as_ptr() as *const UnsafeCell<()> as *const () }
     }
 
     unsafe fn data_mut(&self) -> *mut () {
